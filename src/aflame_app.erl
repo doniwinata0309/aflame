c44b26f4397fba83ef8fe6b05058a77954856710
doni.winata <doni.winata@traveloka.com>
2018-08-24 12:57:07 +0700

    lager

--- src/aflame_app.erl
+++ src/aflame_app.erl
@@ -6,7 +6,8 @@
 
 start(_StartType, _StartArgs) ->
     aflame_web:start(),
-    aflame_sup:start_link().
+    aflame_sup:start_link(),
+    lager:info("Listening to localhost:4000").
 
 stop(_State) ->
     ok.
